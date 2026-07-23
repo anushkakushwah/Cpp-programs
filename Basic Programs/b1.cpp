732da7f0c31964cbc491516266a5c76edd809053
anushkakushwah <anushkaofficial183@gmail.com>
2026-07-23 23:33:32 +0530

    Greatest of 3 without conditions

---
+++ Basic Programs/b1.cpp
@@ -0,0 +1,13 @@
+//to find greater of three numbers without using any conditonal or loop statements
+#include<iostream>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    int a, b, c;
+    cout << "Enter three numbers: ";
+    cin >> a >> b >> c;
+    int greatest = max(a, max(b, c));
+    cout << "Greatest number = " << greatest;
+    return 0;
+}
