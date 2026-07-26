d7dcee91eb44e74cbd1fcc6276d11a6f20fbc639
anushkakushwah <anushkaofficial183@gmail.com>
2026-07-26 20:39:12 +0530

    Cel to Fah

---
+++ Basic Programs/b9.cpp
@@ -0,0 +1,11 @@
+//to convert temperature celcius into fahrenheit
+#include <iostream>
+using namespace std;
+int main() {
+    float tempc, tempf;
+    cout << "Enter temperature in Celsius: ";
+    cin >> tempc;
+    tempf = (1.8 * tempc) + 32;
+    cout << "Temperature in Fahrenheit: " << tempf;
+    return 0;
+}
