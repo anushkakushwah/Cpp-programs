7b58bcb002ecd99a0056f14264c6f193d6ec50d4
anushkakushwah <anushkaofficial183@gmail.com>
2026-09-02 12:06:57 +0530

    ascii of char

---
+++ Basic Programs/b15.cpp
@@ -0,0 +1,10 @@
+//write a program to find ascii value of a character.
+#include<iostream>
+using namespace std;
+int main(){
+    char a;
+    cout << "Enter any character of your choice:" << endl;
+    cin >> a;
+    cout << "ASCII value of character is:" << int(a);
+    return 0;
+}
