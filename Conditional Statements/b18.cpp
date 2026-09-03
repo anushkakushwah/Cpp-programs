d3be09f27f3743ab85d97319ce18703bd89c94cd
anushkakushwah <anushkaofficial183@gmail.com>
2026-09-03 11:45:49 +0530

    Vowel consonant

---
+++ Conditional Statements/b18.cpp
@@ -0,0 +1,15 @@
+//program to check whether the character is vowel or consonant
+#include<iostream>
+using namespace std;
+int main(){
+    char ch;
+    cout << "Enter any character of your choice:" << endl;
+    cin >> ch;
+    if ((ch=='a' || ch=='e' || ch=='i' || ch=='o' || ch=='u') ||(ch=='A' || ch=='E' || ch=='I' || ch=='O' || ch=='U') ){
+        cout << "Your character is vowel." << endl;
+    }else{
+        cout << "Your character is consonant." << endl;
+    }
+
+    return 0;
+}
