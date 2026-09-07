fcbb30c4dd371534790e2c3808c1947a41eb1c95
anushkakushwah <anushkaofficial183@gmail.com>
2026-09-07 11:51:48 +0530

    1 to 100 series

---
+++ Loops/b37.cpp
@@ -0,0 +1,12 @@
+//To print numbers from 1 to 100
+#include<iostream>
+using namespace std;
+int main(){
+    int i,n;
+    cout << "Enter n:" << endl;
+    cin >> n;
+    for(i=1; i<=n; i++){
+        cout << i << endl;
+    }
+    return 0;
+}
