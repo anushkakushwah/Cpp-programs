6aae80b0c9defdb7133b73b9c764b27c53f6d252
anushkakushwah <anushkaofficial183@gmail.com>
2026-09-10 16:42:00 +0530

    Sum of even numbers 1 to n

---
+++ Loops/b64.cpp
@@ -0,0 +1,16 @@
+// print all even numbers from 1 to N
+#include<iostream>
+using namespace std;
+int main(){
+    int n, i ,sum = 0;
+    cout << "Enter N:";
+    cin >> n;
+    for(i=1; i<=n; i++){
+        if(i%2==0){
+            cout <<i << " " << endl;
+            sum = sum+i;
+        }
+    }
+    cout << "Sum of even numbers is:" << sum <<endl;
+    return 0;
+}
