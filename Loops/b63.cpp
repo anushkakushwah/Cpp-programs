b0abf3d44f8f68066655475e8f41df1c17e19364
anushkakushwah <anushkaofficial183@gmail.com>
2026-09-10 16:39:38 +0530

    Odd numbers between 1 to N

---
+++ Loops/b63.cpp
@@ -0,0 +1,14 @@
+// print all odd numbers from 1 to N
+#include<iostream>
+using namespace std;
+int main(){
+    int n, i;
+    cout << "Enter N:";
+    cin >> n;
+    for(i=1; i<=n; i++){
+        if(i%2!=0){
+            cout <<i << " ";
+        }
+    }
+    return 0;
+}
