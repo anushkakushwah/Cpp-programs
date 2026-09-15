3319f15f7be30da8ef79a49fbe8acbdcd1591a3f
anushkakushwah <anushkaofficial183@gmail.com>
2026-09-15 17:08:13 +0530

    RIGHT ALIGN inverted right angle triangle

---
+++ Patterns/b78.cpp
@@ -0,0 +1,20 @@
+// Printing an RIGHT Aligned inverted right angle triangle
+#include<iostream>
+using namespace std;
+int main(){
+    int n,j,i;
+    cout << "Enter no of rows:" ; 
+    cin >> n;
+    for(i=1; i<=n; i++){
+        for(j=1; j<=i-1; j++){
+            cout << " ";
+        }
+        for(j=n; j>=i; j--){
+            cout << "*";
+        }
+        cout << "\n" ;
+    }
+
+
+    return 0;
+}
