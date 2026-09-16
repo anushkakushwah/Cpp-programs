f8f817f26a447a216300e290970e1cf2fcb1aaef
anushkakushwah <anushkaofficial183@gmail.com>
2026-09-16 20:06:08 +0530

    Number Triangle

---
+++ Patterns/b84.cpp
@@ -0,0 +1,15 @@
+// printing number triangle
+#include<iostream>
+using namespace std;
+int main(){
+    int rows, i, j;
+    cout << "No of Rows:" ;
+    cin >> rows;
+    for(i=1; i<=rows; i++){
+        for(j=1; j<=i; j++){
+            cout << j;
+        }
+        cout << "\n";
+    }
+    return 0;
+}
