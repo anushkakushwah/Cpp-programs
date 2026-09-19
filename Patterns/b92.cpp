27db867bff5187e64480bac9248a9973bce51004
anushkakushwah <anushkaofficial183@gmail.com>
2026-09-19 10:29:32 +0530

    Butterfly Pattern

---
+++ Patterns/b92.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n, i, j;
+    cout << "Enter n: ";
+    cin >> n;
+    // Upper half
+    for(i = 1; i <= n; i++){
+        for(j = 1; j <= i; j++){
+            cout << "*";
+        }
+        for(j = 1; j <= 2*n - 2*i; j++){
+            cout << " ";
+        }
+        for(j = 1; j <= i; j++){
+            cout << "*";
+        }
+        cout << "\n";
+    }
+    // Lower half
+    for(i = n-1; i >= 1; i--){
+        for(j = 1; j <= i; j++){
+            cout << "*";
+        }
+        for(j = 1; j <= 2*n - 2*i; j++){
+            cout << " ";
+        }
+        for(j = 1; j <= i; j++){
+            cout << "*";
+        }
+        cout << "\n";
+    }
+    return 0;
+}
