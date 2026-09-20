ec229596a05d9b24196c673ad32a08af2bcdbe71
anushkakushwah <anushkaofficial183@gmail.com>
2026-09-20 21:02:22 +0530

    Reverse triangle pattern with numbers

---
+++ Patterns/b101.cpp
@@ -0,0 +1,15 @@
+// Reverse triangle pattern with numbers
+#include<iostream>
+using namespace std;
+int main(){
+    int n,i,j;
+    cout << "Enter n:";
+    cin >>  n;
+    for(i=1; i<=n; i++){
+        for(j=i; j>=1; j--){
+            cout << j;
+        }
+        cout << "\n";
+    }
+    return 0;
+}
