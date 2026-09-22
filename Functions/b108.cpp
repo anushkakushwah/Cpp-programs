a84c6bd42680a530b09ebb0332ea567510841a40
anushkakushwah <anushkaofficial183@gmail.com>
2026-09-22 21:30:13 +0530

    Factorial func

---
+++ Functions/b108.cpp
@@ -0,0 +1,17 @@
+// Write a function to calculate factorial
+#include<iostream>
+using namespace std;
+int factorial(int n){
+    int fact =1;
+    for(int i =1; i<=n; i++){
+        fact*=i;
+    }
+    return fact;
+}
+int main(){
+    int n;
+    cout << "Enter number:" ;
+    cin >> n;
+    cout << factorial(n);
+    return 0;
+}
